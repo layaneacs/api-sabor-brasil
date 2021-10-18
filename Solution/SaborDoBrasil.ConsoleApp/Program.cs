eef2f6f22e27c85056894a6d5ae0d3b10f3af21d
beatrizsepriano <beatrizsepriano@gmail.com>
2021-10-18 20:10:14 -0300

    Iniciando Implementação

---
+++ Solution/SaborDoBrasil.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using SaborDoBrasil.Dominio;
+using System;
+
+namespace SaborDoBrasil.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Class1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Solution/SaborDoBrasil.Dominio/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SaborDoBrasil.Dominio
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Hello time 12");
+        }
+        
+    }
+}
