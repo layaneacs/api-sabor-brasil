75657f21ac39d8004061016b2a6441c3b38dad64
beatrizsepriano <beatrizsepriano@gmail.com>
2021-10-18 21:05:23 -0300

    Inserindo classes no modelo

---
+++ Solution/SaborDoBrasil.Dominio/Modelo/Ingrediente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaborDoBrasil.Dominio.Modelo
+{
+    public class Ingrediente: EntidadeBase
+    {
+        public string Nome { get; set; }
+        public string Fornecedor { get; set; }
+        public string Fabricante { get; set; }
+    }
+}
