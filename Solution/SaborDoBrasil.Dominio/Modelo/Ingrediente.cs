47bb644205ba1a75a6f11cc52ccadaa616dc74bf
Layane Andreia da Costa Silva <layane.andreia@hotmail.com>
2021-10-22 19:41:42 -0300

    Iniciando testes de unidade em ingredientes

--- Solution/SaborDoBrasil.Dominio/Modelo/Ingrediente.cs
+++ Solution/SaborDoBrasil.Dominio/Modelo/Ingrediente.cs
@@ -9,5 +9,14 @@ namespace SaborDoBrasil.Dominio.Modelo
         public string Nome { get; set; }
         public string Fornecedor { get; set; }
         public string Fabricante { get; set; }
+        public DateTime Validade { get ; set; }
+
+
+        public bool Valido()
+        {
+            return DateTime.Now < this.Validade;
+        }
+
+        
     }
 }
